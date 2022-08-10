3bfe301d6a64e83ce82173fb461dc46241071dd8
unknown <Administrator@KANINI-LTP-485.gisbizdev.gisbiz.chennai.com>
2022-08-10 12:53:23 +0530

    dashboard api done

---
+++ Controllers/DashboardController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using RegisterationAPI.BankingModel;
+
+namespace RegisterationAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DashboardController : ControllerBase
+    {
+        private readonly BankingContext db;
+        public DashboardController(BankingContext _db)
+        {
+            db = _db;
+        }
+
+        [HttpGet]
+        [Route("{accno}")]
+        public async Task<IActionResult> ViewProfileByAccno(string accno)
+        {
+            UserAccount c = await db.UserAccounts.FindAsync(accno);
+            return Ok(c);
+        }
+    }
+}
